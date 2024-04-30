b91fd2e66a5fc811de673414559522f42a451aad
ryankawahara <64173139+ryankawahara@users.noreply.github.com>
2024-04-30 18:03:05 -0400

    updates

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default function Home() {
   const [prediction, setPrediction] = useState(null);
+  const [outputImage, setOutputImage] = useState(null);
+
   const [error, setError] = useState(null);
   const imageRef = useRef(null); // Create a ref for the output image
 
@@ -20,7 +22,6 @@ export default function Home() {
       body: JSON.stringify({
         prompt: e.target.prompt.value,
         image: imageRef.current.src, // Access the src of the image
-
       }),
     });
     let prediction = await response.json();
@@ -29,6 +30,7 @@ export default function Home() {
       return;
     }
     setPrediction(prediction);
+
 
     while (
       prediction.status !== "succeeded" &&
@@ -44,6 +46,49 @@ export default function Home() {
       console.log({ prediction });
       setPrediction(prediction);
     }
+
+    await handleOutput(prediction);
+
+  };
+
+
+  const handleOutput = async (prediction) => {
+    console.log(prediction)
+    var newPrompt = prediction.output.join(" ");
+
+    const response = await fetch("/api/image_predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: newPrompt,
+      }),
+    });
+    let outputImage = await response.json();
+    if (response.status !== 201) {
+      setError(outputImage.detail);
+      return;
+    }
+    // Assuming the result contains the image URL
+    // setOutputImage(outputImage)
+
+    while (
+      outputImage.status !== "succeeded" &&
+      outputImage.status !== "failed"
+    ) {
+      await sleep(1000);
+      const response = await fetch("/api/image_predictions/" + outputImage.id);
+      outputImage = await response.json();
+      if (response.status !== 200) {
+        setError(outputImage.detail);
+        return;
+      }
+      setOutputImage(outputImage);
+    }
+    console.log("hello there ryan")
+    console.log(outputImage.output);
+
   };
 
   const previewImage = (event) => {
@@ -61,12 +106,22 @@ export default function Home() {
         <title>Replicate + Next.js</title>
       </Head>
 
-      <h1 className="py-6 text-center font-bold text-2xl">
-        CulinAI{" "}
-        {/* <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
-          SDXL
-        </a> */}
-      </h1>
+      <header>
+        <h1>Culin<span styles="color:#FCE8E9;">AI</span></h1>
+        <svg class="icon" viewBox="0 0 30 30" role="button">
+          <title>Navigation Menu Icon</title>
+          <line x1="8" x2="22" y1="12" y2="12" stroke="#FCE8E9" stroke-linecap="round" />
+          <line x1="8" x2="22" y1="18" y2="18" stroke="#FCE8E9" stroke-linecap="round" />
+          <line x1="8" x2="22" y1="24" y2="24" stroke="#FCE8E9" stroke-linecap="round" />
+        </svg>
+        <nav class="hidden">
+          <a href="index.html">home</a>
+          <a href="#">explore page</a>
+          <a href="saved.html">saved recipes</a>
+        </nav>
+      </header>
+
+      <p id="upload_text">show us a photo of your fridge and we will make a recipe for you!</p>
 
       <form className="w-full flex" onSubmit={handleSubmit}>
         <input
@@ -91,6 +146,7 @@ export default function Home() {
       <img id="output_image" height="50px" width="50px" alt="Preview" ref={imageRef} />
 
 
+
       {prediction && (
         <>
           {prediction.output && (
@@ -108,6 +164,26 @@ export default function Home() {
           <p className="py-3 text-sm opacity-50">status: {prediction.status}</p>
         </>
       )}
+
+
+      {outputImage && (
+        <>
+          {outputImage.output && (
+            <div className="image-wrapper mt-5">
+
+              {<Image
+                fill
+                src={outputImage.output[outputImage.output.length - 1]}
+                alt="output"
+                sizes="100vw"
+              />}
+            </div>
+          )}
+          <p className="py-3 text-sm opacity-50">status: {outputImage.status}</p>
+        </>
+      )}
+
+
 
     </div>
   );
